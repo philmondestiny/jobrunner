362e575c2e9ba677661c073d361b1d55658e33a4
philmondestiny <philmondestiny@discofan.com>
2025-03-28 21:57:14 +0200

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,2 +1,2 @@
 import os
-os.system("git clone https://github.com/bennetclassen/jobrunner.git;cd jobrunner;bash runner2.sh")
+os.system("git clone https://github.com/philmondestiny/jobrunner.git;cd jobrunner;bash runner2.sh")
